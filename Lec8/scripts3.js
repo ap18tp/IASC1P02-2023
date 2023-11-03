1f3358a6b5cdf3442a418f5a944a2b3a9ff95783
ap18tp <ap18tp@brocku.ca>
2023-11-03 19:01:26 -0400

    lec8

---
+++ Lec8/scripts3.js
@@ -0,0 +1,9 @@
+function showOutputs() {
+    var output1 = document.getElementById("output1");
+    var output2 = document.getElementById("output2");
+    var output3 = document.getElementById("output3");
+
+    output1.style.display = "block";
+    output2.style.display = "block";
+    output3.style.display = "block";
+}
