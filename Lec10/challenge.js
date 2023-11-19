a0417dc1ff381daa8dd72519594285459866282d
ap18tp <ap18tp@brocku.ca>
2023-11-18 20:15:47 -0500

    challenge 10

---
+++ Lec10/challenge.js
@@ -0,0 +1,31 @@
+function add() {
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    var result = num1 + num2;
+    document.getElementById("output").innerText = result;
+}
+
+function subtract() {
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    var result = num1 - num2;
+    document.getElementById("output").innerText = result;
+}
+
+function multiply() {
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    var result = num1 * num2;
+    document.getElementById("output").innerText = result;
+}
+
+function divide() {
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    var result = num1 / num2;
+    document.getElementById("output").innerText = result;
+}
+
+function clearOutput() {
+    document.getElementById("output").innerText = "Output Here";
+}
